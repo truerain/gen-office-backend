6eb405386e864708318602f436339f7622735f83
gituser <relay67@gmail.com>
2026-01-20 20:16:57 +0900

    기본코드

---
+++ src/main/java/com/third/gen_backoffice/GenBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.third.gen_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/third/gen_backoffice/GenBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.third.gen_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
